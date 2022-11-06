9d10c31f671193ef23a77a909023e654c36d8d99
JoacoM <joaquinmenendezt@gmail.com>
2022-11-06 18:06:10 -0300

    Finalizacion entrega

--- js/cart.js
+++ js/cart.js
@@ -1,93 +1,188 @@
-const urlCart = `https://japceibal.github.io/emercado-api/user_cart/25801.json`;
-let cart = []
+const url = "https://japceibal.github.io/emercado-api/user_cart/25801.json";
+const urlCart = `https://japceibal.github.io/emercado-api/products/${localStorage.getItem('carrito')}.json`;
+const cartform = document.getElementById('cart-form');
+let radioCreditPay = document.getElementById("creditCard");
+let radioTransferPay = document.getElementById("transfer");
+let cardNumberInput = document.getElementById("cardNumber");
+let securityNumberInput = document.getElementById("securityNumber");
+let cardExpirationInput = document.getElementById("cardExpiration");
+let accountNumberInput = document.getElementById("accountNumber");
+let cart = [];
+let cartproduct = [];
+const validExpiration= /^(0[1-9]|1[0-2])(\/|-)(2[2-9])$/;
 
-fetch(urlCart) //Comienzo la peiticion de la informacion a la url
-.then(function(urlResult){ //Hace la coneccion con el servidor y le decimos que transforme la informacion en un tipo Json
-    return urlResult.json() 
-})
-.then(function(jsonResult){ //Aqui devuelve los datos obtenidos de la url luego de pasarlos a tipo Json
-    cart = jsonResult;
-    let htmlToAppend = `
-    <div class="text-center p-4">
-        <h2>Carrito de compras</h2>
-    </div>
-    <br>
-    <h4>Articulos a comprar</h4>
-    <table class="table">
-        <thead>
-            <tr>
-                 <th scope="col"></th>
-                <th scope="col">Nombre</th>
-                <th scope="col">Costo</th>
-                <th scope="col">Cantidad</th>
-                <th scope="col">Subtotal</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <th scope="row"><img src=${cart.articles[0].image} width="50px"></img></th>
-                <td>${cart.articles[0].name}</td>
-                <td>${cart.articles[0].currency + " " + cart.articles[0].unitCost}</td>
-                <td><input type="number" id="input" value="${cart.articles[0].count}" style="width: 50px;"></input></td>
-                <td><b id="subtotal"></b></td>
-            </tr>
-        </tbody>
-    </table>
-    <hr>
-        `
-    document.getElementById("userCart").innerHTML += htmlToAppend
-    input = document.getElementById("input");
-    let subtotal = document.getElementById("subtotal");
-    let subtotal2 = document.getElementById("subtotal2")
-    let total = document.getElementById("total");
-    let costoenvio = document.getElementById("costoenvio");
-    
-    input.addEventListener("input",function(){
-        subtotal.innerHTML = cart.articles[0].currency + " " + input.value * cart.articles[0].unitCost
-        subtotal2.innerHTML = input.value * cart.articles[0].unitCost
+fetch(url)
+  .then(function (respuesta) {
+    return respuesta.json()
+  })
+  .then(function (datos) {
+    cart = datos;
+    showInfoCart();
 
-    document.getElementById('option1').addEventListener('click', function() {
-        costoenvio.innerHTML = cart.articles[0].currency + " " + ((input.value *  cart.articles[0].unitCost)*0.15)
-        total.innerHTML = cart.articles[0].currency + " " + ((input.value *  cart.articles[0].unitCost) + ((input.value *  cart.articles[0].unitCost)*0.15))
-    })
-    
-    document.getElementById('option2').addEventListener('click', function() {
-        costoenvio.innerHTML = cart.articles[0].currency + " " + ((input.value *  cart.articles[0].unitCost)*0.07)
-        total.innerHTML = cart.articles[0].currency + " " + ((input.value *  cart.articles[0].unitCost) + ((input.value *  cart.articles[0].unitCost)*0.07))
-    })
-    
-    document.getElementById('option3').addEventListener('click', function() {
-        costoenvio.innerHTML = cart.articles[0].currency + " " + ((input.value *  cart.articles[0].unitCost)*0.05)
-        total.innerHTML = cart.articles[0].currency + " " + ((input.value *  cart.articles[0].unitCost) + ((input.value *  cart.articles[0].unitCost)*0.05))
-    })
-})
+  });
 
 
-document.getElementById('tarjeta').addEventListener('click', function() {
-    pago = document.getElementById("formadepago")
-    document.getElementById('numtarjeta').disabled = false;
-    document.getElementById('codseguridad').disabled = false;
-    document.getElementById('vencimiento').disabled = false;
-    document.getElementById('numcuenta').disabled = true;
-    pago.innerHTML = "Tarjeta de Credito"
+function showInfoCart() {
+  let htmlContentToAppend =  `
+        <tr>
+            <th scope="row"><img src=${cart.articles[0].image} width="50px"></img></th>
+            <td>${cart.articles[0].name}</td>
+            <td>${cart.articles[0].currency + " " + cart.articles[0].unitCost}</td>
+            <td><input type="number" id="inputarticle" style="width: 50px;" value="" min="1"></input>
+            <div class="text-danger small mt-1 visually-hidden" id="checkAlert">
+                Debe seleccionar al menos un artículo.
+            </div></td>
+            <td><b id="subtotal"></b></td>
+        </tr> 
+    `;
+  document.getElementById('cart').innerHTML += htmlContentToAppend;
+  let input = document.getElementById('inputarticle');
+  let subtotal = document.getElementById('subtotal');
+  let cartCost = document.getElementById('cartCost');
+  let shippingCost = document.getElementById('shippingCost');
+  let premiumradio = document.getElementById('premiumradio');
+  let expressradio = document.getElementById('expressradio');
+  let standardradio = document.getElementById('standardradio');
+  let totalCostCart = document.getElementById('totalCostCart');
+  premiumradio.addEventListener('input', function (e){
+    if(cart.articles[0].currency=='USD'){
+    let subtotalValue = input.value * cart.articles[0].unitCost
+    shippingCost.innerHTML = cart.articles[0].currency + " " +(subtotalValue*0.15)
+    totalCostCart.innerHTML = cart.articles[0].currency + " " +(subtotalValue+(subtotalValue*0.15))
+  }
+  else {
+    let subtotalValue = input.value * cart.articles[0].unitCost /41.8
+    shippingCost.innerHTML = 'USD' + " " +(subtotalValue*0.15)
+    totalCostCart.innerHTML = 'USD' + " " +(subtotalValue+(subtotalValue*0.15))
+  }
+  })
+  expressradio.addEventListener('input', function(e){
+    if(cart.articles[0].currency=='USD'){
+    let subtotalValue = input.value * cart.articles[0].unitCost
+    shippingCost.innerHTML = cart.articles[0].currency + " " +(subtotalValue*0.07)
+    totalCostCart.innerHTML = cart.articles[0].currency + " " +(subtotalValue+(subtotalValue*0.07))
+  }
+  else{
+    let subtotalValue = input.value * cart.articles[0].unitCost/41.8
+    shippingCost.innerHTML = 'USD' + " " +(subtotalValue*0.07)
+    totalCostCart.innerHTML = 'USD' + " " +(subtotalValue+(subtotalValue*0.07))
+  }
+  })
+  standardradio.addEventListener('input', function(e){
+    if(cart.articles[0].currency=='USD'){
+    let subtotalValue = input.value * cart.articles[0].unitCost
+    shippingCost.innerHTML = cart.articles[0].currency + " " +(subtotalValue*0.05)
+    totalCostCart.innerHTML = cart.articles[0].currency + " " +(subtotalValue+(subtotalValue*0.05))
+  }
+  else{
+    let subtotalValue = input.value * cart.articles[0].unitCost/41.8
+    shippingCost.innerHTML = cart.articles[0].currency + " " +(subtotalValue*0.05)
+    totalCostCart.innerHTML = cart.articles[0].currency + " " +(subtotalValue+(subtotalValue*0.05))
+  }
+  })
+  
+  input.addEventListener('input', function (e) { 
+    if(cart.articles[0].currency=='USD'){
+    let subtotalValue = input.value * cart.articles[0].unitCost
+    subtotal.innerHTML = cart.articles[0].currency + " " + subtotalValue;
+    cartCost.innerHTML = cart.articles[0].currency + " " + subtotalValue;
+    if (expressradio.checked)
+    {
+      shippingCost.innerHTML = cart.articles[0].currency + " " +(subtotalValue*0.07)
+      totalCostCart.innerHTML = cart.articles[0].currency + " " +(subtotalValue+(subtotalValue*0.07))
+    }
+    else if (standardradio.checked){
+      shippingCost.innerHTML = cart.articles[0].currency + " " +(subtotalValue*0.05)
+      totalCostCart.innerHTML = cart.articles[0].currency + " " +(subtotalValue+(subtotalValue*0.05))
+    }
+   else if (premiumradio.checked){
+    shippingCost.innerHTML = cart.articles[0].currency + " " +(subtotalValue*0.15)
+    totalCostCart.innerHTML = cart.articles[0].currency + " " +(subtotalValue+(subtotalValue*0.15))
+   };
+  }
+  else{
+    let subtotalValue = input.value * cart.articles[0].unitCost/41.8
+    subtotal.innerHTML = 'USD' + " " + subtotalValue;
+    cartCost.innerHTML = 'USD' + " " + subtotalValue;
+    if (expressradio.checked)
+    {
+      shippingCost.innerHTML = 'USD' + " " +(subtotalValue*0.07)
+      totalCostCart.innerHTML = 'USD' + " " +(subtotalValue+(subtotalValue*0.07))
+    }
+    else if (standardradio.checked){
+      shippingCost.innerHTML = 'USD' + " " +(subtotalValue*0.05)
+      totalCostCart.innerHTML = 'USD' + " " +(subtotalValue+(subtotalValue*0.05))
+    }
+   else if (premiumradio.checked){
+    shippingCost.innerHTML = 'USD' + " " +(subtotalValue*0.15)
+    totalCostCart.innerHTML = 'USD' + " " +(subtotalValue+(subtotalValue*0.15))
+   };
+  }
+
+  })
+};
+
+  function validField(ok,field){
+      if (ok) {
+        document.getElementById(`${field}`).classList.remove('is-invalid');
+        document.getElementById(`${field}`).classList.add('is-valid');
+      } else {
+        document.getElementById(`${field}`).classList.remove('is-valid');
+        document.getElementById(`${field}`).classList.add('is-invalid');
+      };
+  }
+  const form=document.getElementById("cart-info");
+  form.addEventListener("submit", function(e){
+      e.preventDefault();
+      validField(form.adressnumber.value!="","adressnumber");
+      validField(form.adressstreet.value!="","adressstreet");
+      validField(form.adresscorner.value!="","adresscorner");
+      validField(form.inputarticle.value!=""&&form.inputarticle.value>0,"inputarticle");
+      validField(form.premiumradio.checked||form.expressradio.checked||form.standardradio.checked,"standardradio");
+      if (!document.getElementById("creditCard").checked&&!document.getElementById("transfer").checked) {
+        document.getElementById("checkbox").classList.add("link-danger")
+        validField(false,"creditCard");
+        validField(false,"transfer");
+        document.getElementById("checkAlert").classList.remove('visually-hidden');
+    } else if(document.getElementById("creditCard").checked&&!document.getElementById("transfer").checked){
+        if(cardNumberInput.value!=""&&securityNumberInput.value!=""&&validExpiration.test(cardExpirationInput.value))
+        {document.getElementById("checkbox").classList.remove("link-danger")};
+        validField(true,"creditCard");
+        validField(false,"transfer");
+        validField(cardNumberInput.value!="","cardNumber");
+        validField(securityNumberInput.value!="","securityNumber");
+        validField(cardExpirationInput.value!=""&&cardExpirationInput.value.length<6&&validExpiration.test(cardExpirationInput.value),"cardExpiration");
+        document.getElementById("checkAlert").classList.add('visually-hidden');
+    }
+    else{
+      if(accountNumberInput.value!="")
+        {document.getElementById("checkbox").classList.remove("link-danger")};
+      validField(false,"creditCard");
+      validField(true,"transfer");
+      validField(accountNumberInput.value!="","accountNumber");
+      document.getElementById("checkAlert").classList.add('visually-hidden');
+
+    }
+    if (form.adressnumber.value!=""&&form.adressstreet.value!=""&&form.inputarticle.value!=""&&form.inputarticle.value>0&&
+    form.adresscorner.value!=""&&(radioCreditPay.checked||radioTransferPay.checked)&&
+    (form.premiumradio.checked||form.expressradio.checked||form.standardradio.checked)
+    &&((cardNumberInput.value!=""&&securityNumberInput.value!=""&&validExpiration.test(cardExpirationInput.value))||accountNumberInput.value!=""))
+    {
+    document.getElementById("good").classList.remove('visually-hidden'); 
+  }});
+  
+  
+  radioCreditPay.addEventListener('click', function(e) {
+    cardNumberInput.disabled = false; 
+    securityNumberInput.disabled = false;
+    cardExpirationInput.disabled = false;
+    accountNumberInput.disabled = true;
   });
   
-document.getElementById('transferencia').addEventListener('click', function() {
-    pago = document.getElementById("formadepago")
-    document.getElementById('numtarjeta').disabled = true;
-    document.getElementById('codseguridad').disabled = true;
-    document.getElementById('vencimiento').disabled = true;
-    document.getElementById('numcuenta').disabled = false;
-    pago.innerHTML = "Transferencia bancaria"
+  
+   radioTransferPay.addEventListener('click', function(e) {
+    cardNumberInput.disabled = true; 
+    securityNumberInput.disabled = true;
+    cardExpirationInput.disabled = true;
+    accountNumberInput.disabled = false;
   });
-})
-
-function cambioMoneda (){
-    let valor = cart.articles[0].unitCost
-    if (cart.articles[0].currency = "USD"){
-        valor = cart.articles[0].unitCost / 40
-        return valor
-    } else {
-        return valor
-    }
-}
